77c29d2cd637d5a6c424109080256fd2761213b2
Камалиев Робин <kamaliev@benefitty.ru>
2024-02-14 20:13:40 +0500

    Добавление DTO для получения продуктов.

--- app/Services/Api/Models/Products/Repositories/EloquentProductRepository.php
+++ app/Services/Api/Models/Products/Repositories/EloquentProductRepository.php
@@ -6,6 +6,7 @@ namespace App\Services\Api\Models\Products\Repositories;
 
 use App\Filters\ProductFilter;
 use App\Models\Product;
+use App\Services\Api\Models\Products\Dto\ProductFilterDto;
 use Illuminate\Contracts\Pagination\Paginator;
 
 /**
@@ -16,10 +17,10 @@ final class EloquentProductRepository implements ProductRepositoryInterface
     /**
      * Получить продукты.
      */
-    public function getProducts(array $data): Paginator
+    public function getProducts(ProductFilterDto $productFilterDto): Paginator
     {
         /** @var ProductFilter $productFilter */
-        $productFilter = app(ProductFilter::class, ['data' => $data]);
+        $productFilter = app(ProductFilter::class, ['data' => $productFilterDto->toArray()]);
 
         return Product::filter($productFilter)
             ->with('properties')
